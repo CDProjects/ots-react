8072db612c0267f33f5cc9c6009b67dab86e9fdc
CDProjects <caseydentprojects@gmail.com>
2024-07-03 09:40:28 +0300

    App.js amended to render app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import News from './Components/News';
-import Team from './Components/Team';
-import Training from './Components/Training';
-import Juniors from './Components/Juniors';
-import Fixtures from './Components/Fixtures';
-import MedRec from './Components/MedRec';
-import Contact from './Components/Contact';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/training" element={<Training />} />
-          <Route path="/juniors" element={<Juniors />} />
-          <Route path="/fixtures" element={<Fixtures />} />
-          <Route path="/medrec" element={<MedRec />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {/* Your other components and routes go here */}
       </div>
     </Router>
   );
