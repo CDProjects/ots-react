2fe4ed8a7ba609fd8283eb4fb3e299c8cd3429c6
CDProjects <caseydentprojects@gmail.com>
2024-07-03 12:45:36 +0300

    Home carousel, images, logo, title and paragraph applied max view port width

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
       <div className="App">
         <div className="content-container">
           <Navbar />
+          <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Other routes */}
+        </Routes>
           {/* Your routes and other components go here */}
         </div>
       </div>
