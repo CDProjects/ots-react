05bae0cf5f9376dd57c1993ad3786c07822a55d6
CDProjects <caseydentprojects@gmail.com>
2024-07-04 21:19:53 +0300

    Sponsor logos adjusted smaller view ports

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,20 +9,21 @@ import artomikaLogo from '../Images/Artomika.png';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <h3>Proudly sponsored by:</h3>
-        <div className="sponsor-images">
+      <div className="sponsor-images">
+        <div className="sponsor-row top-row">
+          <a href="https://lindoselementit.fi/" target="_blank" rel="noopener noreferrer">
+            <img src={lindosLogo} alt="Lindos" className="sponsor-image lindos" />
+          </a>
+        </div>
+        <div className="sponsor-row bottom-row">
+          <a href="https://www.rocktape.com" target="_blank" rel="noopener noreferrer">
+            <img src={rocktapeLogo} alt="RockTape" className="sponsor-image" />
+          </a>
           <a href="https://www.uudenmaanlukko.fi/" target="_blank" rel="noopener noreferrer">
             <img src={paavilainenLogo} alt="Paavilainen" className="sponsor-image" />
           </a>
           <a href="https://www.uudenmaanlukko.fi/" target="_blank" rel="noopener noreferrer">
             <img src={uudenmaanLogo} alt="Uudenmaan" className="sponsor-image" />
-          </a>
-          <a href="https://www.rocktape.com" target="_blank" rel="noopener noreferrer">
-            <img src={rocktapeLogo} alt="RockTape" className="sponsor-image" />
-          </a>
-          <a href="https://lindoselementit.fi/" target="_blank" rel="noopener noreferrer">
-            <img src={lindosLogo} alt="Lindos" className="sponsor-image" />
           </a>
           <img src={artomikaLogo} alt="Artomika" className="sponsor-image" />
         </div>
