7951d07752f2e34422db89fabe64752fe8bb58ff
CDProjects <caseydentprojects@gmail.com>
2024-07-10 13:29:50 +0300

    Updates

---
+++ static/js/482.b4b931bd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkots_react=self.webpackChunkots_react||[]).push([[482],{4482:(s,a,e)=>{e.r(a),e.d(a,{default:()=>c});e(5043);var t=e(579);const c=s=>{let{src:a,alt:e,className:c}=s;return(0,t.jsx)("img",{src:a,alt:e,className:c,loading:"lazy"})}}}]);
+//# sourceMappingURL=482.b4b931bd.chunk.js.map
